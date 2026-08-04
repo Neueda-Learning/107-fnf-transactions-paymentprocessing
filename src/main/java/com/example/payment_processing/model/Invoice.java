d36c9e2b817a92e4732c3ce7a2734a37d5f66103
Shrihari004 <shriharith004@gmail.com>
2026-08-04 09:15:44 +0000

    added invoice creation API and supporting components including controller, service, DTO, repository, and entity updates

--- src/main/java/com/example/payment_processing/model/Invoice.java
+++ src/main/java/com/example/payment_processing/model/Invoice.java
@@ -1,18 +1,11 @@
 package com.example.payment_processing.model;
 
 import jakarta.persistence.*;
-import lombok.*;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "invoices")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-
 public class Invoice {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,5 +20,47 @@ public class Invoice {
     @ManyToOne
     @JoinColumn(name="vendor_id", nullable=false)
     private Vendor vendor;
+
+    public Invoice() {
+    }
+
+    public Invoice(Long id, String invoiceNumber, BigDecimal invoiceAmount, Vendor vendor) {
+        this.id = id;
+        this.invoiceNumber = invoiceNumber;
+        this.invoiceAmount = invoiceAmount;
+        this.vendor = vendor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+
+    public BigDecimal getInvoiceAmount() {
+        return invoiceAmount;
+    }
+
+    public void setInvoiceAmount(BigDecimal invoiceAmount) {
+        this.invoiceAmount = invoiceAmount;
+    }
+
+    public Vendor getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(Vendor vendor) {
+        this.vendor = vendor;
+    }
 
 }
